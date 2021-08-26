74ee79e8de5c51a57dc656a0049f6c5c9da53c59
varun.belekar@somaiya.edu <varun.belekar@somaiya.edu>
2021-08-26 12:18:16 +0530

    first commit

---
+++ hexagonal-springboot/src/main/java/com/hexagonal/HexagonalSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.hexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexagonalSpringbootApplication.class, args);
+	}
+
+}
